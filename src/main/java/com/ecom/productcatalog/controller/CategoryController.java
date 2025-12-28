24d566bbadfd71a6c5e4cbbb64ddd78a28ef9d5a
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-12-28 22:34:54 +0430

    first

--- src/main/java/com/ecom/productcatalog/controller/CategoryController.java
+++ src/main/java/com/ecom/productcatalog/controller/CategoryController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/categories")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:5173")
 public class CategoryController {
 
     private final CategoryService categoryService;
--- src/main/java/com/ecom/productcatalog/controller/ProductController.java
+++ src/main/java/com/ecom/productcatalog/controller/ProductController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/products")
+@CrossOrigin("http://localhost:5173")
 public class ProductController {
 
     private final ProductService productService;
